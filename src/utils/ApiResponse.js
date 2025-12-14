32beb01ac4d09879614ab262118c418f14073ede
Meet Jain <meetjain@Meets-MacBook-Air-2.local>
2025-12-14 19:31:14 +0530

    utils file created

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,15 @@
+class ApiResponse {
+  constructor(
+    statusCode,
+    message = "success",
+    data,
+  )
+  {
+    this.data = data,
+    this.statusCode.statusCode
+    this.message = message
+    this.success = statusCode < 400
+  }
+}
+
+export {ApiResponse}
